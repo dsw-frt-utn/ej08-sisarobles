26ae91e3a2dbd7f57b72dd7275e4c82c1ed08b91
Sisa Robles <SisaVeronicaRobles@alu.frt.utn.edu.ar>
2026-05-03 00:01:40 -0300

    commit problema 4

---
+++ Dsw2026Ej8/Problema N°4/Problema4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_N_4
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            int notasValidas = 0;
+
+            ProcesarNota(nota1, ref suma, ref notasValidas);
+            ProcesarNota(nota2, ref suma, ref notasValidas);
+            ProcesarNota(nota3, ref suma, ref notasValidas);
+
+            if (notasValidas == 0)
+            {
+                return 0;
+            }
+
+            return suma / notasValidas;
+        }
+
+        private void ProcesarNota(int? nota, ref double suma, ref int contador)
+        {
+            if (nota != null && nota.Value >= 0 && nota.Value <= 10)
+            {
+                suma += nota.Value;
+                contador++;
+            }
+        }
+        
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dsw2026Ej8.Problema_N_1;
 using Dsw2026Ej8.Problema_N_2;
 using Dsw2026Ej8.Problema_N_3;
+using Dsw2026Ej8.Problema_N_4;
 
 namespace Dsw2026Ej8
 {
@@ -8,6 +9,8 @@ namespace Dsw2026Ej8
     {
         static void Main(string[] args)
         {
+            //PROBLEMA 1
+            Console.WriteLine("=== PROBLEMA 1 ===");
             ProductHelper helper = new ProductHelper();
 
             long codigo = 1024;
@@ -19,26 +22,35 @@ namespace Dsw2026Ej8
             Console.WriteLine("Etiqueta generada:");
             Console.WriteLine(etiqueta);
 
+            //PROBLEMA 2
+            Console.WriteLine("\n=== PROBLEMA 2 ===");
             Class1 p2 = new Class1();
             string resultado = p2.CrearResumenVenta(505, "Teclado Mecánico", 2, 50000m);
             Console.WriteLine(resultado);
 
-            Console.WriteLine("\n--- Resultado Problema 3 ---");
-
+            //PROBLEMA 3
+            Console.WriteLine("\n=== PROBLEMA 3 ===");
             Problema3 p3 = new Problema3();
             Product miProducto = new Product();
 
             int miNumero = 100;
-
             miProducto.SetDescription("Descripción Original");
-
             string resultadoP3 = p3.CompararCopias(miNumero, miProducto);
 
             Console.WriteLine(resultadoP3);
+            Console.WriteLine($"Comprobación:");
+            Console.WriteLine($"Número original: {miNumero} (No cambió)");
+            Console.WriteLine($"Descripción: {miProducto.GetDescription()} (Sí cambió)");
 
-            Console.WriteLine($"\nComprobación en Main:");
-            Console.WriteLine($"Número original en Main: {miNumero} (No cambió)");
-            Console.WriteLine($"Descripción en Main: {miProducto.GetDescription()} (¡Sí cambió!)");
+            //PROBLEMA 4
+            Console.WriteLine("\n=== PROBLEMA 4 ===");
+            Problema4 p4 = new Problema4();
+
+            double promedio = p4.CalcularPromedio(8, 15, null);
+
+            Console.WriteLine($"Promedio Final: {promedio}");
+
+            //PROBLEMA 5
 
             Console.WriteLine("\nPresiona cualquier tecla para salir...");
             Console.ReadKey();
